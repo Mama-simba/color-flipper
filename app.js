429c70d083e1d8f0b2c3004c4d909de528a8f23c
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2022-01-31 15:54:07 +0100

    function randonNumber created

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
 
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
+const button = document.getElementById("btn"); // selects button
+const color = document.getElementById(".color"); // selects the color code
+
+
+button.addEventListener("click", changeColor);
+
+function changeColor(event){
+
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    //color.textContent = colors[randomNumber];
+
+}
+
+function getRandomNumber(){
+    // Get random number between 0 - 3, as we have 4 colors in the array
+    return Math.floor(Math.random() * 4);
+
+}
