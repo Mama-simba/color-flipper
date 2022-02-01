d891f171d0949c0644a914136f91ef430efac49e
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2022-02-01 15:32:29 +0100

    Hex functionality done

--- hex.js
+++ hex.js
@@ -1,5 +1,4 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-    "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const button = document.getElementById("btn"); // selects button
 const color = document.querySelector(".color"); // selects the color code
@@ -9,7 +8,7 @@ button.addEventListener("click", changeColor);
 function changeColor(event){
     let hexColor = "#";
     for(let i = 0; i < 6; i++){
-        hexColor += hex[0];
+        hexColor += hex[getRandomNumber()];
     }
     document.body.style.backgroundColor = hexColor;  // selects the background
     color.textContent = hexColor;  // selects the color text
@@ -18,6 +17,6 @@ function changeColor(event){
 
 function getRandomNumber(){
     // Get random number between 0 - 3, as we have 4 colors in the array
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * hex.length);
 
 }
