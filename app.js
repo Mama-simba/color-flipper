7e37babdcfaaf9bf270332fbbce7d8f7a1b540cb
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2022-02-01 09:46:07 +0100

    simple functionality done

--- app.js
+++ app.js
@@ -3,17 +3,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const button = document.getElementById("btn"); // selects button
-const color = document.getElementById(".color"); // selects the color code
+const color = document.querySelector(".color"); // selects the color code
 
 
 button.addEventListener("click", changeColor);
 
 function changeColor(event){
-
     const randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = colors[randomNumber];
-    //color.textContent = colors[randomNumber];
-
+    document.body.style.backgroundColor = colors[randomNumber];  // selects the background
+    color.textContent = colors[randomNumber];  // selects the color text
 }
 
 function getRandomNumber(){
